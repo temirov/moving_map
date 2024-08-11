858ace7f99c94a886325cbdb95b060241d0a513d
Vadym Tyemirov <temirov@gmail.com>
2024-08-11 14:08:23 -0700

    Batching the retrieval of the counties

--- scripts/countyWorker.js
+++ scripts/countyWorker.js
@@ -2,9 +2,17 @@ self.onmessage = function() {
     fetch('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json')
         .then(response => response.json())
         .then(countiesData => {
-            self.postMessage({ type: 'counties', data: countiesData });
+            let batchSize = 100; // Define a batch size for chunked processing
+            for (let i = 0; i < countiesData.features.length; i += batchSize) {
+                // Slice the batch and send it to the main thread
+                let batch = countiesData.features.slice(i, i + batchSize);
+                batch.forEach(feature => {
+                    self.postMessage({ type: 'county', data: feature });
+                });
+            }
+            self.postMessage({ type: 'complete' });
         })
         .catch(error => {
-            console.error('Error loading counties data:', error);
+            self.postMessage({ type: 'error', message: error.message });
         });
 };
